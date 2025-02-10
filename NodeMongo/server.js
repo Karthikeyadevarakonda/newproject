673b4479c25fca5e4b8d20c3c106240d3ba80f74
Karthikeyadevarakonda <161142507+Karthikeyadevarakonda@users.noreply.github.com>
2025-02-10 21:26:58 +0530

    Add files via upload

---
+++ NodeMongo/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const dotEnv = require('dotenv')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const cors = require('cors');
+
+const employeeRoutes = require('./routes/employeeRouters')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+dotEnv.config()
+
+app.use(bodyParser.json())
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{ 
+    console.log("DB CONNECTED....!")
+})
+.catch((Error)=>{
+    console.log("ERROR",Error)
+})
+
+
+app.use('/employees',employeeRoutes)
+
+
+app.listen(PORT,()=>{
+    console.log("SERVER CONNECTED.....!")
+})
